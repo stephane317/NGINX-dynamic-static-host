5a74d82368172dd8af963bef385d81d12b15c0d9
stephane317 <stephane.nimer@hotmail.fr>
2019-11-26 15:42:50 +0100

    Clean

--- landing/e-cotiz/test/e-cotiz/script/form-creation.js
+++
@@ -1,9 +0,0 @@
-//FORM CREATION
-let searchParams = new URLSearchParams(window.location.search)
-searchParams.has('mail')
-
-let mail = searchParams.get('mail')
-let mailField = document.querySelector('#mail')
-mailField.value = mail
-
-console.log(mailField.value);
